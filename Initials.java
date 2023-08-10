bf200590013bea1627168c4fe3f3d44cc9cff258
yeungemy <emyzheng80@gmail.com>
2023-08-10 19:42:42 -0400

    added second prj

---
+++ Initials.java
@@ -0,0 +1,17 @@
+public class Initials {
+    public static void main(String[] args) {
+        String[] lines = {
+            " SSS     A  ",
+            "S   S   A A ",
+            "s      A   A",
+            " SSS   AAAAA",
+            "    S  A   A",
+            "S   s  A   A",
+            " SSS   A   A"
+        };
+
+        for(Integer i = 0; i < lines.length; i++){
+            System.out.println(lines[i]);
+        }
+    }
+}
