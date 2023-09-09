19b3a1f02f8a7d595e0247739f1e0cdaf3d3696a
yeungemy <emyzheng80@gmail.com>
2023-09-08 23:07:51 -0400

    crud

--- src/OOP/bookStore/Book.java
+++ src/OOP/bookStore/Book.java
@@ -1,5 +1,86 @@
 package OOP.bookStore;
 
+import java.text.DecimalFormat;
+import java.util.Objects;
+
 public class Book {
-    
+    private String title;
+    private String author;
+    private double rating;
+    private double price;
+
+    /** creat a constructor that initializes the fields of a newly created object  
+     * @param {String} title - the book title
+     * @param {String} author - the author of the book
+     * @param {double} rating - the rating of the book
+     * @param {double} price - the price of the book
+     */
+    public Book(String title, String author, double rating, double price){
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    /** create a constructor that initializes a new object using an existing object's fields 
+     * @param {Book} source - an existing book object
+     */
+    public Book(Book source){
+        this.title = source.title;
+        this.author = source.author;
+        this.rating = source.rating;
+        this.price = source.price;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getRating() {
+        return this.rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString(){
+        return "'" + this.title + "' written by " + this.author + " is rated " + new DecimalFormat("#.##").format(this.rating) + " and sold for $" + new DecimalFormat("#.##").format(this.price); 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Book)) {
+            return false;
+        }
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && rating == book.rating && price == book.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, rating, price);
+    } 
 }
--- src/OOP/bookStore/Store.java
+++ src/OOP/bookStore/Store.java
@@ -1,5 +1,43 @@
 package OOP.bookStore;
+import java.util.ArrayList;
 
 public class Store {
-    
+    private ArrayList<Book> books;
+
+    /** create a constructor for the Store class that initializes an empty ArrayList of Book objects */
+    public Store(){
+        this.books = new ArrayList<Book>();
+    }
+
+    /** Create a getBook method that takes an index as a parameter 
+     * @param {int} index - an index as a parameter
+     * @return returns a deep copy of the Book object at that index
+     */
+    public Book getBook(int index){
+        return this.books.get(index);
+    }
+
+    /** Create a setBook method that takes a Book object and an index as parameters and sets the Book object at the specified index to a deep copy of the given object  
+     * @param {int} index - an index as a parameter
+     * @param {Book} book - the Book object
+     */
+    public void setBook(int index, Book book){
+        this.books.set(index, book);
+    }
+
+    public void addBook(Book book){
+        this.books.add(book);
+    }
+
+    public boolean contains(Book book){
+        return this.books.contains(book);
+    }
+
+    public void sellBook(String title){
+        for(int i = 0; i < this.books.size(); i++){
+            if(this.books.get(i).getTitle().equals(title)){
+                this.books.remove(i);
+            }
+        }
+    }   
 }
