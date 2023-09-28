b607e5d385aa00b1f6b7182ace04593cc3452ff8
Emy Yeung <emyzheng80@gmail.com>
2023-09-28 12:16:01 -0400

    updated

--- projects/array/reverseArray/src/Main.java
+++ projects/array/reverseArray/src/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         final int[] myArr = {1, 2, 3, 4, 5, 6};
 
+        System.out.println("Array: " + Arrays.toString(myArr));
+
         ReverseArray reversedArray = new ReverseArray();
 
         int[] reversedArr = reversedArray.reverse(myArr);
+               
+        System.out.println("Reversed Array: " + Arrays.toString(reversedArr));
 
-        Arrays.stream(reversedArr).forEach(System.out::println);
+        // Arrays.stream(reversedArr).forEach(System.out::println);
     }
 }
