bed898b1e2939e4af6962e2a42fa9f588f07ce1b
Emy Yeung <emyzheng80@gmail.com>
2023-09-28 11:16:12 -0400

    removed hard code

--- projects/minElement/src/Main.java
+++ projects/minElement/src/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MinimumElement minEle = new MinimumElement();
+        final int size = 2;
 
-        int[] myArr = minEle.readElements(sc,5);
+        int[] myArr = minEle.readElements(sc, size);
         int minEl = minEle.findMin(myArr);
 
         System.out.println("Minimum Ele: " + minEl);
